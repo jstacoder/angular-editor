5df8167c7fc3271ff11a3a5a24172d461237fc6d
Kyle Roux <kyle@level2designs.com>
2015-04-27 18:04:54 -0700

    app.py changes

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from mongoengine import Document,EmbeddedDocument,ReferenceField,EmbeddedDocumentField,StringField,ListField
-from flask import Flask,request,session,send_file,make_response
+from mongoengine import Document,EmbeddedDocument,ReferenceField,EmbeddedDocumentField,StringField,ListField,connect,queryset
+from flask import Flask,request,session,send_file,make_response,Blueprint,jsonify
+import json
 
-app = Flask(__name__,static_folder='./',template_folder='./')
+app = Flask(__name__,static_folder='./',template_folder='./',static_url_path='')
 
+
+api = Blueprint('api',__name__,url_prefix='/api/v1')
+
+
+connect('newdb')
 
 @app.route('/')
 def index():
@@ -22,4 +28,21 @@ class File(Document):
     content = StringField()
 
 
-app.run(host='0.0.0.0',port=5555)
+@api.route('/user',methods=['GET','POST'])
+def user():
+    if request.method.upper() == 'GET':
+        objects = User.objects.all()
+        return jsonify(o=[x.to_json() for x in objects])
+        
+    else:
+        data = json.loads(request.data)
+        try:
+            u = User.objects.get(username=data['username'])
+        except:
+            u = User(**data)
+            u.save()
+        return jsonify(user=u.to_json())
+
+app.register_blueprint(api)
+
+app.run(host='0.0.0.0',port=5555,debug=True)
