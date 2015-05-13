f05a628a97bb6d0a8a748579ffa3b5209c5be090
kyle roux <kyle@level2designs.com>
2015-05-13 08:05:49 -0700

    fixs + migrations

--- new.coffee
+++ new.coffee
@@ -1,6 +1,12 @@
 'use strict'
 
-app = angular.module 'editor.app',['ngRoute','ui.ace','mgcrea.ngStrap','ui.bootstrap','auth.app']
+app = angular.module 'editor.app',[
+  'ngRoute'
+  'ui.ace'
+  'mgcrea.ngStrap'
+  'ui.bootstrap'
+  'auth.app'
+]
 
 app.constant 'apiPrefix','/api/v1'
 
@@ -12,6 +18,35 @@ app.factory 'aceLoaded',()->
         console.log _rend,_sess,_editor
         return
 
+app.directive 'closeBtn',(removeFile)->
+    restrict:'E'
+    template:"<span class='close'>X</span>"
+    link:(scope,ele,attrs)->
+        console.log attrs
+        proj_id = attrs['projId']
+        ele.on 'click', ()->
+            ele.parent().remove()
+            removeFile proj_id
+            return
+        return
+
+app.filter 'update',()->
+    return (data)->
+        return data
+
+app.filter 'count',()->
+    return (data)->
+        if angular.isObject(data) then Object.keys(data).length else data.length
+
+app.factory 'removeProject',($http,apiPrefix)->
+    return (proj_id)->
+        return $http.post "#{apiPrefix}/delete/project",
+            object_id:proj_id
+
+app.factory 'removeFile',($http,apiPrefix)->
+    return (file_id)->
+        return $http.post "#{apiPrefix}/delete/document",
+            object_id:file_id
 
 app.factory 'aceCfg',(aceLoaded)->
     rtn =
@@ -61,16 +96,36 @@ app.directive 'tzNav',()->
     restrict:"E"
     templateUrl:"navbar.html"
     replace:true
+    require:"tzNav"
+    link:($scope,ele,attrs,authService)->
+      $scope.$on('auth:logout',()->
+          authService.reset()
+          return
+      )
+      $scope.status = {}
+      $scope.status.isopen = false
+      $scope.updateAuth = ()->
+          $scope.authenticated = authService.hasData()
+          return
+      $scope.updateAuth()
+      $scope.getUserName = ()->
+        return authService.getData().username
+      $scope.getEmail = ()->
+          if authService.getData().emails and authService.getData().emails.length > 0
+              return authService.getData().emails[0]
+      $scope.getAvatar = ()->
+        return authService.getData().avatar
+      return
 
 
 app.factory 'viewCount',($http,apiPrefix)->
     return $http.get "#{apiPrefix}/viewcount"
 
-app.factory 'collections',(collectUsers,collectProjects)->
-    return (itm)->
+app.factory 'collections',(collectUsers,projectService)->
+  return (itm)->
         rtn =
             user:collectUsers
-            project:collectProjects
+            project:projectService.getProjects
         return rtn[itm]()
 
 
@@ -86,6 +141,78 @@ app.config ($routeProvider,$locationProvider)->
         templateUrl:'home.html'
         controller:($scope)->
             return
+    ).when('/logout',
+            controller:($rootScope,logout)->
+                $rootScope.$broadcast 'auth:logout'
+                return logout()
+    ).when('/login',
+      templateUrl:'auth.html'
+      controller:($scope,login,$uiModal)->
+        $scope.newuser =
+            username:''
+            email:''
+            password:''
+            confirm:''
+          $scope.user =
+            email:''
+            password:''
+        $scope.login = ()->
+            login($scope.user.email,$scope.user.password).then (res)->
+                
+                modal = $uiModal.open
+                    templateUrl: 'myLoginModal.html'
+                    scope:$scope.$new()
+
+                modal.result.then (res)->
+                    console.log res
+                    return
+                
+        $scope.loginMode = true
+        $scope.setLoginMode = ()->
+            $scope.registerMode = false
+            $scope.loginMode = true
+            return
+        $scope.setRegisterMode = ()->
+            $scope.registerMode = true
+            $scope.loginMode = false
+            return
+          $scope.isActiveClass = (type)->
+            map =
+              register:$scope.registerMode
+              login:$scope.loginMode
+            return map[type]
+        return
+    ).when('/register',
+      templateUrl:'auth.html'
+      controller:($scope,register)->
+          $scope.register = ()->
+              register($scope.newuser.username,$scope.newuser.password,$scope.newuser.email).then (res)->
+                  console.log res
+                  return
+              return
+          $scope.newuser =
+            username:''
+            email:''
+            password:''
+            confirm:''
+          $scope.user =
+            email:''
+            password:''
+          $scope.registerMode = true
+          $scope.setLoginMode = ()->
+            $scope.registerMode = false
+            $scope.loginMode = true
+            return
+          $scope.setRegisterMode = ()->
+            $scope.registerMode = true
+            $scope.loginMode = false
+            return
+          $scope.isActiveClass = (type)->
+            map =
+              register:$scope.registerMode
+              login:$scope.loginMode
+            return map[type]
+          return
     ).when('/test',
         templateUrl:'test.html'
         controller:($scope,collectProjects,collectUsers)->
@@ -103,11 +230,19 @@ app.config ($routeProvider,$locationProvider)->
             return
     ).when('/:item/list',
         templateUrl:'list.html'
-        controller:($uiModal,$scope,$routeParams,collectProjects,collectUsers,collections,addProject)->
+        controller:($uiModal,$scope,$routeParams,collectProjects,collectUsers,collections,addProject,removeProject,authService,projectService)->
             console.log $routeParams
             console.log $routeParams.item
             $scope.item = $routeParams.item
-            $scope.checkMode = ($event)->
+            #collectProjects().then (r)->
+            #  console.log('result---->',r)
+            #  $scope.coll = r
+            #  return
+            updateColl = ()->
+              $scope.coll = projectService.getProjects()
+              return
+            updateColl()
+            $scope.checkMode = ($event,proj_id)->
                 console.log($event)
                 if $scope.removeMode
                     $event.preventDefault()
@@ -121,7 +256,14 @@ app.config ($routeProvider,$locationProvider)->
                     modal.result.then( (res)->
                         console.log res
                         angular.element($event.srcElement).remove()
+                        projectService.removeProject(proj_id).then (res)->
+                            if not res
+                                $scope.removeError = true
+                            return
                         $scope.removeMode = false
+                        
+                        updateColl()
+                        console.log projectService.getProjects()
                         return
                     ,(err)->
                         console.log err
@@ -134,19 +276,21 @@ app.config ($routeProvider,$locationProvider)->
                 return
             $scope.testVar = 'testing 1,2,3'
             $scope.items = ['x','y','a']
-            collections($scope.item).then (res)->
-                console.log res
-                $scope.coll = res
+            #collections($scope.item).then (res)->
+            #    console.log res
+            #    $scope.coll = res
             $scope.getUrl = (itm)->
                 return "#{$scope.item}/profile/#{itm._id.$oid}"
             $scope.addProject = ()->
+                userData = authService.getData()
+                console.log('User:--->',userData)
                 _s = $scope.$new({testVar:'cccccc'})
                 _s.testVar = 'ttttttttt'
                 modal = $uiModal.open
                     controller:($scope,$modalInstance)->
                         $scope.project = {}
                         $scope.project.name = ''
-                        
+
                     controllerAs:'ng-controller as ctrl'
                     title:'testModal'
                     templateUrl: 'myModalContent.html'
@@ -154,7 +298,7 @@ app.config ($routeProvider,$locationProvider)->
                         items:()->
                             return $scope.items
                 modal.result.then( (res)->
-                    console.log res
+                    console.log('proj data--->',res)
                     addProject(res).then (r)->
                         console.log r
                         $scope.coll.push r.data.object
@@ -168,7 +312,7 @@ app.config ($routeProvider,$locationProvider)->
             return
     ).when('/:item/profile/:id',
         templateUrl:'profile.html'
-        controller:($uiModal,$scope,$routeParams,collections,collectUsers,collectProjects,addFile,back)->
+        controller:($uiModal,$scope,$routeParams,collections,collectUsers,collectProjects,addFile,back,removeFile)->
             colls =
                 user:collectUsers
                 project:collectProjects
@@ -177,6 +321,17 @@ app.config ($routeProvider,$locationProvider)->
             console.log $scope.item
             $scope.route_id = $routeParams.id
             console.log $scope.route_id
+            $scope.deleteConfirm = (file_id)->
+                removeFile(file_id).then (res)->
+                    if res
+                        collectProjects().then (r)->
+                            $scope.coll = r
+                            angular.forEach $scope.coll, (itm)->
+                                if itm._id.$oid == parseInt $scope.route_id
+                                    $scope.profile = itm
+                                return
+                    return
+                return
             $scope.addFile = ()->
                 modal = $uiModal.open
                     controller:($scope,$modalInstance)->
@@ -195,9 +350,9 @@ app.config ($routeProvider,$locationProvider)->
                     console.log err
                     return
                 )
-                    
+
             $scope.getProjectName = (p)->
-                project(p.$oid).then (res)->
+                project(p._id.$oid).then (res)->
 
             if $scope.item == 'user'
                 collectUsers().then (res)->
@@ -252,7 +407,7 @@ app.config ($routeProvider,$locationProvider)->
             setCfg = (opts)->
                 angular.extend aceCfg, opts
                 $scope.cfg = aceCfg
-            
+
             File($routeParams.id).then (res)->
                     console.log res.data
                     $scope.file = res.data
@@ -275,10 +430,12 @@ app.factory 'addFile',($http,apiPrefix)->
             name:name
             project_id:pid
 
-app.factory 'addProject',($http,apiPrefix)->
-    return (name)->
+app.factory 'addProject',($http,apiPrefix,authService)->
+    user_id = authService.getData().id
+    return (name)->      
         return $http.post "#{apiPrefix}/create/project",
             name:name
+            user:user_id
 
 
 
@@ -286,12 +443,17 @@ app.factory 'collectProjects',($q,projects)->
     _projs = []
     def = $q.defer()
     projects().then (res)->
-        console.log(res.data.objects)
-        angular.forEach res.data.objects,(itm)->
+
+        console.log('projects',res.data.projects)
+        angular.forEach res.data.projects,(itm)->
             _itm = angular.fromJson itm
-            console.log _itm
+            console.log('fixed: ', _itm)
             _projs.push _itm
-        return def.resolve _projs
+            return
+        if _projs.length > 0
+          def.resolve _projs
+        else
+          def.reject 'no projects'
     return ()->
         def.promise
 
@@ -336,7 +498,7 @@ app.factory 'saveFile',($http,apiPrefix)->
     return (oid,data)->
         data['id'] = oid
         return $http.post "#{apiPrefix}/save" , data
-            
+
 app.factory 'File',($http,apiPrefix)->
     return (oid)->
         return $http.get "#{apiPrefix}/file/#{oid}"
@@ -357,6 +519,39 @@ app.factory 'projects',($http,apiPrefix)->
     return ()->
         return $http.get "#{apiPrefix}/project"
 
-
-
-
+app.service 'projectService',($q,Project,projects,addProject,removeProject)->
+  self = @  
+  _projects = []
+    
+  projects().then (res)->
+    angular.forEach res.data.projects,(itm)->
+      _projects.push itm
+      return
+    return
+  self.getProjects = ()->
+    return _projects
+  self.getProject = (pid)->
+    return Project pid
+  self.addProject = (name)->
+    addProject(name).then (res)->
+      if obj = res.data.object
+        _projects.push obj
+      return
+    return
+  self.removeProject = (pid)->
+    def = $q.defer()
+    removeProject(pid).then( (res)->
+      if res.data.result
+        idx = undefined
+        angular.forEach _projects,(itm,_idx)->
+          if itm._id.$oid == pid
+            idx = _idx
+        if not angular.isUndefined(idx)
+          console.log('splicing idx',idx)
+          _projects.splice(idx,1)
+          console.log _projects
+        def.resolve(res.data.result)
+        return
+    )    
+    return def.promise
+  return self
--- sql_app.py
+++ sql_app.py
@@ -1,26 +1,85 @@
-#from mongoengine import Document,EmbeddedDocument,ReferenceField,EmbeddedDocumentField,StringField,ListField,connect,queryset,DictField,IntField
 from flask import Flask,request,session,send_file,make_response,Blueprint,jsonify,g
 from redis import Redis
-from functools import partial
+from functools import partial,wraps
 import os
 import jwt
 import json
 import pickle
 import requests
-from appdb import User,Project,Document
+from appdb import User,Project,Document,Email
 
 cache = Redis()
+
+MODEL_DICT = dict(
+    file=Document,
+    document=Document,
+    project=Project,
+    user=User,
+    email=Email,
+)
+
+class Name(object):
+    name = 'Fail'
+
+nme = Name()
+def secure_route(f):
+    @wraps(f)
+    def wrapper(*args,**kwargs):
+        headers = request.headers
+        auth = headers.get('Authorization',None)
+        if auth is not None:
+            print auth
+            token = auth.split(' ')[-1]
+            if not token == 'null':
+                user_data = jwt.decode(token,str(session.get('user_id',None)))
+                if int(user_data.get('id')) == session.get('user_id'):
+                    g.user = User.get(user_data.get('id'))
+                    print g.get('user',nme).name
+        return f(*args,**kwargs)
+    return wrapper
+
+def get_token(user):
+    return jwt.encode(
+                    dict(
+                        username = user.name,
+                        id = user.id,
+                        emails = [
+                            x.address for x in user.emails
+                        ],
+                        avatar = user.gravatar_url,
+                    ),
+                    str(user.id)
+    )
+
+def read_token(token,key):
+    return jwt.decode(token,key)
+
+def _read_token(token,key):
+    return jwt.decode(token,key,verify=False)
+
+def safe_read_token(token,key):
+    try:
+        return read_token(token,key)
+    except jwt.exceptions.DecodeError:
+        return _read_token(token,key)
+
 
 class RequestCache(object):
     _cache = cache
 
+    def _make_key(self,data):
+        data = data[1:] if data[0] is '/' else data
+        return data.replace('/',':').replace('?','')
+
     def _set(self,key,val,ex):
+        key = self._make_key(key)
         self._cache.hset('cache_map',key,val)
         self._cache.set(key,val)
         self._cache.expire(key,ex)
 
     def _get(self,key):
-        return self._cache.get(key)    
+        key = self._make_key(key)
+        return self._cache.get(key)
 
     def get(self,key):
         if self._has(key):
@@ -53,8 +112,10 @@ app = Flask(__name__,static_folder='./',template_folder='./',static_url_path='')
 app.config['SECRET_KEY'] = 'shhh'
 
 
-frontend = Blueprint('front',__name__,url_prefix='/test',subdomain='test-domain')
+frontend = Blueprint('front',__name__,url_prefix='')
 api = Blueprint('api',__name__,url_prefix='/api/v1')
+
+
 
 
 @app.before_request
@@ -86,31 +147,36 @@ def check_request_cache():
 
 @app.after_request
 def af(r):
-    r.make_sequence()
-    r.implicit_sequence_conversion = True
-    data = r.get_data(as_text=True)
+    
+    from coffeescript import compile as coffee
+    r.direct_passthrough = False
+    #r.implicit_sequence_conversion = True
+    data = r.data
+    if len(request.url.split('.')) > 1 and request.path.endswith('.coffee'):
+        data = coffee(data)
+        r = make_response(data)
     if session.get('CACHE_REQUEST',False):
         print 'setting cache for {}'.format(session.get('CACHE_KEY'))
         _c.set(session.get('CACHE_KEY'),data)
-    #res = r.__dict__.pop('response')
-    #fields = r.__dict__.copy()
-    #r.__dict__['response'] = res
-    #p = pickle.dumps(fields)
+    res = r.__dict__.pop('response')
+    fields = r.__dict__.copy()
+    r.__dict__['response'] = res
+    p = pickle.dumps(data)
     #print p
-    #x = pickle.loads(p)
-    #assert x == p
+    x = pickle.loads(p)
+    assert x == data
     return r
 
 
 @app.before_first_request
 def increment_cache():
-    print request.environ
+    #print request.environ
     ip_key = CACHE_KEY+':remoteip'
     current_ip = request.environ.get('HTTP_X_FORWARDED_FOR',None) or request.environ.get('HTTP_X_REAL_IP')
     last_ip = _get_cache(ip_key) or 'save'
     if last_ip == 'save' or last_ip != current_ip:
         set_cache(ip_key,current_ip)
-        print current_ip
+        #print current_ip
         inc_cache()
     session['current_view_count'] = get_cache()
 
@@ -119,12 +185,6 @@ def vc():
     session['current_view_count'] = get_cache()
     return jsonify(count=session.get('current_view_count',0))
 
-            
-@frontend.route('/')
-def i():
-    return 'hi'
-
-#connect('editor_app2',host='ds055200.mongolab.com',port=55200,username='editor',password=os.environ.get('MONGOHQ_DB_PASSWORD'))
 
 @app.errorhandler(404)
 def error(err):
@@ -132,9 +192,8 @@ def error(err):
         rtn = send_file('index2.html')
         session.just_sent = True
     else:
-        rtn = make_response('')
-    return make_response(''),200
-
+        rtn = (make_response(''),200)
+    return rtn
 
 def login(oid):
     session['user_id'] = oid
@@ -142,86 +201,67 @@ def login(oid):
 def logout():
     session.pop('user_id',None)
 
-'''
-class User(Document):
-    username = StringField(max_length=255,required=True)
-    projects = ListField(ReferenceField('Project'))
-    name = StringField()
-
-    def __init__(self,*args,**kwargs):
-        if not 'name' in kwargs:
-            kwargs.setdefault('name',kwargs.get('username',''))
-        super(Document,self).__init__(*args,**kwargs)
-
-
-
-    def add_project(self,proj=None,**kwargs):
-        if not proj in self.projects:
-            if proj is None:
-                proj = kwargs.pop('project')
-            if type(proj) == dict:
-                proj = Project(**proj)
-            self.projects.append(proj)
-        return self.save()
-'''
+class AnonymuousUser(object):
+    _is_authenticated = False
+    
+    @property
+    def is_authenticated(self):
+        return self._is_authenticated
+    
+    def __init__(self,authenticated=False):
+        self._is_authenticated = authenticated
+    
 
 def get_user():
     oid = session.get('user_id',None)
-    return oid if oid is None else User.session.get(oid)
+    return AnonymuousUser() if oid is None else User.get(oid)
+
+@api.route('/logout',methods=['POST'])
+def _logout():
+    logout()
+    return jsonify(success=True)
+
+
+@api.route('/register',methods=['POST'])
+def reg():
+    data = json.loads(request.data)
+    print data
+    email = data.get('email',False)
+    pw = data.get('password',False)
+    username = data.get('username',False)
+    user_exists = User.query.filter(User.username==username).first() and User.query.join(Email).filter(Email.address==email).first()
+    if user_exists:
+        rtn = dict(result='Error',success=False,reason='user exists')
+    else:
+        user = User(username=username,password=pw,emails=[email])
+        user.save()
+        rtn = dict(result='Success',success=True,reason='user created',obj=user.to_json())
+    return jsonify(rtn)
+
+
+
+
+@api.route('/authenticate',methods=['POST'])
+def _auth_login():
+    data = json.loads(request.data)
+    email = data.get('email',False)
+    pw = data.get('password',False)
+    user = User.query.join(Email).\
+                        filter(Email.address==email).\
+                        first()
+    if user is not None:
+        if user.check_password(pw):
+            login(user.id)
+            rtn = jsonify(token=get_token(user))
+        else:
+            rtn = jsonify(token=False,error="incorrect authentication")
+    else:
+        rtn = jsonify(token=False,error="incorrect authentication")
+    return rtn
 
 @app.route('/')
 def index():
     return send_file('index2.html')
-
-
-'''
-class File(EmbeddedDocument):
-    name = StringField(max_length=255,required=True)
-    content = StringField()
-    project = ReferenceField('Project')
-    oid = IntField()    
-   
-class Project(Document):
-    name = StringField(max_length=255,required=True,unique=True)
-    files = ListField(EmbeddedDocumentField('File'))
-    _file_map = DictField()
-    
-    @classmethod
-    def _get_file(cls,oid):
-        
-            get file with referenced oid regardless of project
-        
-        try:
-            rtn = filter(None,map(lambda x: filter(lambda y: y.oid == int(oid),x.files),cls.objects.all()))[0][0]
-        except IndexError,e:
-            rtn = None
-        return rtn 
-    
-    def has_file(self,name):
-        return name in self._file_map.values()
-
-    def get_file(self,idx=None,name=None):
-        if idx is not None:
-            return self.files[idx]
-        return self.files[self._file_map.values().index(name)]
-
-    def add_file(self,_file):
-        idx = len(self._file_map.keys())
-        if type(_file) == dict:
-            f = File(**_file)
-        else:
-            f = _file
-        self._file_map[str(idx)] = f.name
-        self.update(push__files=f)
-        self.save()
-        #f.project = self
-        self.files.append(f)
-        f.oid = self._get_oid()
-        return self.save()
-    
-    def _get_oid(self):
-        return sum(map(lambda x: len(x.files),list(self.__class__.objects)))    
-'''
 
 @api.route('/file/create',methods=['POST'])
 def _create_file():
@@ -229,9 +269,9 @@ def _create_file():
     f = Document()
     data = json.loads(request.data)
     f.title = data.get('name',None)
-    f.project = Project.session.query(Project).get(data['project'])
-    print f.project
+    Project.get(data['project']).add_file(f)
     f.save()
+    print 'created file'
     return jsonify(result='success',obj=f.to_json())
 
 @api.route('/file/<oid>')
@@ -248,16 +288,20 @@ def _files(pid):
     #    print File.objects.get(id=fle.id)
         rtn.append(fle)
     return jsonify(objects=[f.to_json() for f in rtn])
-        
-        
+
+
+@api.route('/delete/<obj_type>',methods=['POST'])
+def delete(obj_type):
+    result = MODEL_DICT[obj_type].get(json.loads(request.data)['object_id']).delete()
+    return jsonify(result=result or 'fail')
+
 @api.route('/create/<obj_type>',methods=['POST'])
 def create(obj_type):
-    model = dict(
-        document=Document,
-        project=Project,
-        user=User,
-    )[obj_type](**json.loads(request.data))
-    model.save()
+    model = MODEL_DICT[obj_type](
+            **json.loads(
+                request.data
+            )
+    ).save()
     return jsonify(object=model.to_json())
 
 
@@ -280,11 +324,28 @@ def save():
     fle.save()
     return jsonify(result='success',obj=fle.to_json())
 
+
+@api.route('/project',methods=['GET'])
+@secure_route
+def _projects():
+    print g.get('user',nme).name
+    user = get_user()
+    print user.projects.all() if user is not None else ''
+    if user is not None:
+        #print [dir(p) for p in user.projects]
+        projects = [p.to_json() for p in user.projects.all()]# else Project.objects.get(id=p.id).to_json()]
+    else:
+        projects = []
+    return jsonify(projects=projects)
+
+
+
 @api.route('/<obj_type>',methods=['GET'])
+@secure_route
 def obj_type(obj_type):
     model = dict(
         user=User,
-        project=Project,        
+        project=Project,
         document=Document,
     )[obj_type]
     objects = model.session.query(model).all()
@@ -297,7 +358,9 @@ def obj_type(obj_type):
     return res
     #return jsonify(objects=[x.to_json() for x in objects])
 
+
 @api.route('/user',methods=['POST'])
+@secure_route
 def _user():
     data = json.loads(request.data)
     u = User.session.query(User).filter(User.username==data['username'])
@@ -306,29 +369,26 @@ def _user():
         u.save()
     return jsonify(user=u.to_json())
 
+
+
 @api.route('/user/<_id>',methods=['POST','GET'])
+@secure_route
 def _get_user(_id):
     u = User.session.query(User).get(_id)
     return jsonify(user=u.to_json())
 
+
+
 @api.route('/project',methods=['POST'])
+@secure_route
 def _project():
+
     data = json.loads(request.data)
     p = Project.session.query(Project).filter(Project.name==data['name'])
     if p is None:
         p = Project(**data)
         p.save()
     return jsonify(project=p.to_json())
-
-@api.route('/projects',methods=['GET'])
-def _projects():
-    user = get_user()
-    if user is not None:
-        #print [dir(p) for p in user.projects]
-        projects = [p.to_json() for p in user.projects if hasattr(p,'to_json')]# else Project.objects.get(id=p.id).to_json()]
-    else:
-        projects = []
-    return jsonify(projects=projects)
 
 @api.route('/login',methods=['POST'])
 def _login():
@@ -338,11 +398,18 @@ def _login():
 
 app.register_blueprint(api)
 
+@app.route('/fonts/<font_name>')
+def font(font_name):
+    return send_file(os.path.join('fonts',font_name))
+
+
+app.register_blueprint(frontend)
+
 @app.route('/<catch>')
 @app.route('/<catch>/<path:more>')
 @app.route('/<catch>/<more>/<mmore>')
 def catch(catch,more=None,mmore=None):
-    if more is None or ('.html' in catch or '.js' in catch or '.css' in catch):
+    if more is None or ('.html' in catch or '.js' in catch or '.css' in catch or '.ttf' in catch or '.woff' in catch):
         try:
             return send_file(catch)
         except IOError,e:
